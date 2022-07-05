388f7a9ad6eb5d739913ce5706e5936abac73ba0
Sergiy Stasyuk <sergiystasyuk@MacBook-Air-Sergiy.local>
2022-07-05 16:58:14 +0300

    add src and composer.json

---
+++ src/ImageEngineCdn.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace ImageEngine\Cdn;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\ActivateContext;
+use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+
+class ImageEngineCdn extends Plugin
+{
+    public function install(InstallContext $installContext): void
+    {
+        parent::install($installContext);
+    }
+
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        if ($uninstallContext->keepUserData()) {
+            return;
+        }
+    }
+
+    public function activate(ActivateContext $activateContext): void
+    {
+        parent::activate($activateContext);
+    }
+
+    public function deactivate(DeactivateContext $deactivateContext): void
+    {
+        parent::deactivate($deactivateContext);
+    }
+}
