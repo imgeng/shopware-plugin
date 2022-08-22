26a9d3339b805b1e3f825f85c5ab0e7b7d96acd8
Jon Arne S <seteras@gmail.com>
2022-08-22 10:04:24 +0200

    updated name of zip to ScientiaMobileImageEngineCdn and added a dist folder

---
+++ dist/ScientiaMobileImageEngineCdn/src/ScientiaMobileImageEngineCdn.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace ScientiaMobileImageEngine\Cdn;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\ActivateContext;
+use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+
+class ScientiaMobileImageEngineCdn extends Plugin
+{
+    public function install(InstallContext $installContext): void
+    {
+        parent::install($installContext);
+    }
+
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        if ($uninstallContext->keepUserData()) {
+            return;
+        }
+    }
+
+    public function activate(ActivateContext $activateContext): void
+    {
+        parent::activate($activateContext);
+    }
+
+    public function deactivate(DeactivateContext $deactivateContext): void
+    {
+        parent::deactivate($deactivateContext);
+    }
+}
